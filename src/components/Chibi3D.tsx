067583b095440dcb177ba0fb6fd58ed50c3201ec
tunglofichill <creepn2006@gmail.com>
2026-06-16 10:58:20 +0700

    fix: correct 3D geometry rotations for bangs and gas mask

--- src/components/Chibi3D.tsx
+++ src/components/Chibi3D.tsx
@@ -186,8 +186,8 @@ export default function Chibi3D({
 
                   {/* Bangs */}
                   {hairId !== 'hair_curly' && (
-                    <mesh position={[0, 0.6, 0.7]} rotation={[0.2, 0, 0]}>
-                      <capsuleGeometry args={[0.15, 1.2, 16, 16]} rotation={[0, 0, Math.PI / 2]} />
+                    <mesh position={[0, 0.55, 0.8]} rotation={[0.2, 0, Math.PI / 2]}>
+                      <capsuleGeometry args={[0.15, 1.2, 16, 16]} />
                       <meshStandardMaterial color={hairColor} roughness={0.9} />
                     </mesh>
                   )}
@@ -246,19 +246,21 @@ export default function Chibi3D({
                 </mesh>
               )}
               {accessoryId === 'acc_gasmask' && (
-                <mesh position={[0, -0.3, 0.9]}>
-                  <cylinderGeometry args={[0.3, 0.4, 0.4, 16]} rotation={[Math.PI/2, 0, 0]} />
-                  <meshStandardMaterial color="#222" />
+                <group position={[0, -0.2, 0.95]}>
+                  <mesh rotation={[Math.PI/2, 0, 0]}>
+                    <cylinderGeometry args={[0.25, 0.35, 0.2, 16]} />
+                    <meshStandardMaterial color="#222" />
+                  </mesh>
                   {/* Filters */}
-                  <mesh position={[-0.3, 0, 0.2]} rotation={[0, Math.PI/4, 0]}>
-                    <cylinderGeometry args={[0.15, 0.15, 0.2, 16]} rotation={[Math.PI/2, 0, 0]} />
+                  <mesh position={[-0.25, -0.1, 0.1]} rotation={[Math.PI/2, 0, 0.4]}>
+                    <cylinderGeometry args={[0.15, 0.15, 0.15, 16]} />
                     <meshStandardMaterial color="#444" />
                   </mesh>
-                  <mesh position={[0.3, 0, 0.2]} rotation={[0, -Math.PI/4, 0]}>
-                    <cylinderGeometry args={[0.15, 0.15, 0.2, 16]} rotation={[Math.PI/2, 0, 0]} />
+                  <mesh position={[0.25, -0.1, 0.1]} rotation={[Math.PI/2, 0, -0.4]}>
+                    <cylinderGeometry args={[0.15, 0.15, 0.15, 16]} />
                     <meshStandardMaterial color="#444" />
                   </mesh>
-                </mesh>
+                </group>
               )}
               {accessoryId === 'acc_catears' && (
                 <group position={[0, 0.85, 0]}>
