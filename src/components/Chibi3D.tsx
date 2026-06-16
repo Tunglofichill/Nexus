c0dd5dae29521277afaa5c6d0d4f83a4737273a1
tunglofichill <creepn2006@gmail.com>
2026-06-16 10:38:54 +0700

    fix: make 3D chibi actually cute instead of cursed

--- src/components/Chibi3D.tsx
+++ src/components/Chibi3D.tsx
@@ -25,78 +25,103 @@ export default function Chibi3D({
         <pointLight position={[0, 5, -5]} intensity={2} color="#a855f7" />
         
         <Suspense fallback={null}>
-          <group position={[0, -1.5, 0]}>
-            {/* Body */}
-            <mesh position={[0, 0.9, 0]}>
-              <capsuleGeometry args={[0.5, 0.8, 16, 32]} />
-              <meshStandardMaterial color={clothesColor} roughness={0.6} />
+          <group position={[0, -1, 0]}>
+            
+            {/* Body (Cute and small) */}
+            <mesh position={[0, 0.8, 0]}>
+              <capsuleGeometry args={[0.45, 0.6, 16, 32]} />
+              <meshStandardMaterial color={clothesColor} roughness={0.7} />
             </mesh>
 
-            {/* Neck (Hidden by clothes, but gives separation) */}
-            <mesh position={[0, 1.7, 0]}>
-              <cylinderGeometry args={[0.2, 0.2, 0.3, 16]} />
+            {/* Neck */}
+            <mesh position={[0, 1.4, 0]}>
+              <cylinderGeometry args={[0.15, 0.2, 0.2, 16]} />
               <meshStandardMaterial color={skinColor} roughness={0.4} />
             </mesh>
             
-            {/* Head */}
-            <mesh position={[0, 2.3, 0]}>
-              <boxGeometry args={[1.4, 1.3, 1.4]} />
-              {/* Adding a bit of rounded effect by using a sphere or rounded box, but standard box with high segments works too if it was sphere. Let's use sphere for cute chibi */}
-            </mesh>
-
-            <mesh position={[0, 2.3, 0]}>
-              <sphereGeometry args={[0.8, 32, 32]} />
+            {/* Head (Big and round) */}
+            <mesh position={[0, 2.2, 0]}>
+              <sphereGeometry args={[0.9, 32, 32]} />
               <meshStandardMaterial color={skinColor} roughness={0.4} />
             </mesh>
 
-            {/* Eyes */}
-            <group position={[0, 2.3, 0.75]}>
-              <mesh position={[-0.3, 0.1, 0]}>
-                <sphereGeometry args={[0.1, 16, 16]} />
+            {/* Eyes (Big, cute, wide apart but not too much) */}
+            <group position={[0, 2.2, 0.82]}>
+              {/* Left Eye */}
+              <mesh position={[-0.35, 0.1, 0]}>
+                <sphereGeometry args={[0.12, 16, 16]} />
                 <meshBasicMaterial color="#000000" />
               </mesh>
-              <mesh position={[0.3, 0.1, 0]}>
-                <sphereGeometry args={[0.1, 16, 16]} />
+              {/* Left Highlight */}
+              <mesh position={[-0.38, 0.14, 0.1]}>
+                <sphereGeometry args={[0.04, 8, 8]} />
+                <meshBasicMaterial color="#ffffff" />
+              </mesh>
+
+              {/* Right Eye */}
+              <mesh position={[0.35, 0.1, 0]}>
+                <sphereGeometry args={[0.12, 16, 16]} />
                 <meshBasicMaterial color="#000000" />
               </mesh>
-              {/* Eye highlights */}
-              <mesh position={[-0.33, 0.13, 0.08]}>
-                <sphereGeometry args={[0.03, 8, 8]} />
+              {/* Right Highlight */}
+              <mesh position={[0.32, 0.14, 0.1]}>
+                <sphereGeometry args={[0.04, 8, 8]} />
                 <meshBasicMaterial color="#ffffff" />
               </mesh>
-              <mesh position={[0.27, 0.13, 0.08]}>
-                <sphereGeometry args={[0.03, 8, 8]} />
-                <meshBasicMaterial color="#ffffff" />
+            </group>
+
+            {/* Blush (Cute pink cheeks) */}
+            <group position={[0, 2.2, 0.8]}>
+              <mesh position={[-0.5, -0.1, 0]}>
+                <sphereGeometry args={[0.15, 16, 16]} />
+                <meshStandardMaterial color="#ff9999" transparent opacity={0.6} />
+              </mesh>
+              <mesh position={[0.5, -0.1, 0]}>
+                <sphereGeometry args={[0.15, 16, 16]} />
+                <meshStandardMaterial color="#ff9999" transparent opacity={0.6} />
               </mesh>
             </group>
 
             {/* Hair - dynamic based on hairId */}
             {hairId !== 'hair_bald' && (
-              <mesh position={[0, 2.9, -0.1]}>
-                {hairId === 'hair_short' ? (
-                  <boxGeometry args={[1.6, 0.8, 1.6]} />
-                ) : hairId === 'hair_long' ? (
-                  <capsuleGeometry args={[0.8, 1.5, 16, 16]} />
-                ) : (
-                  <sphereGeometry args={[0.9, 16, 16]} />
-                )}
-                
-                {hairId === 'hair_neon' ? (
-                  <meshStandardMaterial color={hairColor} emissive={hairColor} emissiveIntensity={0.8} />
-                ) : (
-                  <meshStandardMaterial color={hairColor} roughness={0.8} />
-                )}
-              </mesh>
-            )}
+              <group position={[0, 2.2, 0]}>
+                {/* Main Hair Volume */}
+                <mesh position={[0, 0.3, -0.1]}>
+                  {hairId === 'hair_short' ? (
+                    <sphereGeometry args={[0.95, 32, 32]} />
+                  ) : hairId === 'hair_long' ? (
+                    <sphereGeometry args={[0.95, 32, 32]} />
+                  ) : (
+                    <sphereGeometry args={[0.95, 32, 32]} />
+                  )}
+                  {hairId === 'hair_neon' ? (
+                    <meshStandardMaterial color={hairColor} emissive={hairColor} emissiveIntensity={0.8} />
+                  ) : (
+                    <meshStandardMaterial color={hairColor} roughness={0.9} />
+                  )}
+                </mesh>
 
-            {/* If long hair, add the back part */}
-            {hairId === 'hair_long' && (
-              <mesh position={[0, 1.8, -0.5]}>
-                <capsuleGeometry args={[0.7, 1.5, 16, 16]} />
-                <meshStandardMaterial color={hairColor} roughness={0.8} />
-              </mesh>
-            )}
+                {/* Bangs (Front part of hair) */}
+                <mesh position={[0, 0.7, 0.5]}>
+                  <capsuleGeometry args={[0.2, 0.8, 16, 16]} />
+                  <meshStandardMaterial color={hairColor} roughness={0.9} />
+                  <group rotation={[0, 0, Math.PI / 2]}>
+                    <mesh position={[0, 0, 0]}>
+                      <capsuleGeometry args={[0.2, 0.8, 16, 16]} />
+                      <meshStandardMaterial color={hairColor} roughness={0.9} />
+                    </mesh>
+                  </group>
+                </mesh>
 
+                {/* Long hair extension */}
+                {hairId === 'hair_long' && (
+                  <mesh position={[0, -0.8, -0.6]} rotation={[-0.2, 0, 0]}>
+                    <capsuleGeometry args={[0.6, 1.5, 16, 16]} />
+                    <meshStandardMaterial color={hairColor} roughness={0.9} />
+                  </mesh>
+                )}
+              </group>
+            )}
           </group>
           <ContactShadows position={[0, -1.5, 0]} opacity={0.5} scale={10} blur={2} far={4} />
           <Environment preset="city" />
